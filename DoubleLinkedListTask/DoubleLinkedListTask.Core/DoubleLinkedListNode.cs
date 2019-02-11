4794763b597bcbcf4d4ba29e2468114bf139c057
Popelenkov Vladlen <popelenkow@yandex.ru>
2019-02-12 01:15:41 +0700

    Add collection

--- DoubleLinkedListTask/DoubleLinkedListTask.Core/DoubleLinkedListNode.cs
+++ DoubleLinkedListTask/DoubleLinkedListTask.Core/DoubleLinkedListNode.cs
@@ -4,15 +4,20 @@ namespace DoubleLinkedListTask.Core
 {
     public class DoubleLinkedListNode<T> : IDoubleLinkedListNode<T>
     {
-        public T Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public T Value { get; set; }
 
-        public IDoubleLinkedListNode<T> Next => throw new NotImplementedException();
+        public IDoubleLinkedListNode<T> Next { get; set; }
 
-        public IDoubleLinkedListNode<T> Previous => throw new NotImplementedException();
+        public IDoubleLinkedListNode<T> Previous { get; set; }
+
+        public DoubleLinkedListNode(T value = default(T))
+        {
+            Value = value;
+        }
 
         public override string ToString()
         {
-            throw new NotImplementedException();
+            return Value.ToString();
         }
     }
 }
