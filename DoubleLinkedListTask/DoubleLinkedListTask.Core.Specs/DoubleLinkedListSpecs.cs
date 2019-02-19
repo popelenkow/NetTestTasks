f3d2c33868ef5bd78c5218d3719beadd6026a950
Vladlen Popelenkov <v.popelenkov@eastbanctech.ru>
2019-02-19 18:03:58 +0700

    Restructure DoubleLinkedListTask

---
+++ DoubleLinkedListTask/DoubleLinkedListTask.Core.Specs/DoubleLinkedListSpecs.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace DoubleLinkedListTask.Core.Specs
+{
+    public class DoubleLinkedListSpecs
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
