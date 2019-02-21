ced08ac003d3d447b770f0afaf79c01f172f988e
Vladlen Popelenkov <v.popelenkov@eastbanctech.ru>
2019-02-21 18:21:19 +0700

    Add BraCketTask

---
+++ BraCketTask/BraCketTask.Core/BracketValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BraCketTask.Core
+{
+    public static class BracketValidator
+    {
+        public static bool Validate(string value, char bra, char cket)
+        {
+            int counter = 0;
+            foreach (var element in value)
+            {
+                if (bra == element) counter++;
+                else if (cket == element) counter--;
+                if (counter < 0) return false;
+            }
+            return counter == 0;
+        }
+    }
+}
