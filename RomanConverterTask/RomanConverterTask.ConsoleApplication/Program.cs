33aae1a72d395f78b665e25ee8060c969ffbf980
Vladlen Popelenkov <v.popelenkov@eastbanctech.ru>
2019-02-22 16:20:11 +0700

    Hot fix

--- RomanConverterTask/RomanConverterTask.ConsoleApplication/Program.cs
+++ RomanConverterTask/RomanConverterTask.ConsoleApplication/Program.cs
@@ -8,15 +8,6 @@ namespace RomanConverterTask.ConsoleApplications
     {
         static void Main(string[] args)
         {
-            var r = File.ReadAllLines(@"C:\Users\v.popelenkov\source\repos\tests\NetTestTasks\RomanConverterTask\RomanConverterTask.ConsoleApplication\ff.txt");
-            var g = r.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-            g = new string[] { "", "M", "MM", "MMM" }.Select(x => g.Select(y => x + y).Append("t")).Aggregate((x, y) => x.Concat(y)).ToArray();
-            for (int i = 0; i < g.Length; i++)
-            {
-                g[i] = "{ " + '"' + g[i] + '"' + ", " + (i + 1).ToString() + " },";
-            }
-           
-            File.WriteAllLines(@"C:\Users\v.popelenkov\source\repos\tests\NetTestTasks\RomanConverterTask\RomanConverterTask.ConsoleApplication\gg.txt", g);
         }
     }
 }
