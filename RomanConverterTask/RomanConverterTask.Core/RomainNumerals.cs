07d2c5bdce6720314cc6067bf4d2f465043b3113
Vladlen Popelenkov <v.popelenkov@eastbanctech.ru>
2019-02-22 16:19:39 +0700

    Add solution RomanConverterTask

---
+++ RomanConverterTask/RomanConverterTask.Core/RomainNumerals.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanConverterTask.Core
+{
+    public static class RomainNumerals
+    {
+        public static char[] OriginalSequenceSymbols => new char[] { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+        public static readonly IRomainNumerals Original = new Implementation(OriginalSequenceSymbols);
+
+        public static IRomainNumerals Create(params char[] increasingSequenceSymbols)
+        {
+            return new Implementation(increasingSequenceSymbols);
+        }
+
+        class Implementation : IRomainNumerals
+        {
+            private readonly char[] _symbols;
+            private readonly IReadOnlyDictionary<char, int> _values;
+
+            public Implementation(params char[] symbols)
+            {
+                _symbols = symbols;
+                _values = symbols.ToDictionary(x => x, x => CalculateValue(x));
+            }
+
+            public bool CanSubtract(char subtrahend, char minuend)
+            {
+                var subtrahendIndex = Array.IndexOf(_symbols, subtrahend);
+                var minuendIndex = Array.IndexOf(_symbols, minuend);
+
+                if (subtrahendIndex == -1) throw new ArgumentOutOfRangeException(nameof(subtrahend), _symbols, string.Empty);
+                if (minuendIndex == -1) throw new ArgumentOutOfRangeException(nameof(minuend), _symbols, string.Empty);
+
+                if (minuendIndex % 2 != 0) return false;
+
+                var range = subtrahendIndex - minuendIndex;
+                return 0 < range && range < 3;
+            }
+
+            public bool IsPowerOfTen(char symbol)
+            {
+                var index = Array.IndexOf(_symbols, symbol);
+
+                if (index == -1) throw new ArgumentOutOfRangeException(nameof(symbol), _symbols, string.Empty);
+
+                return index % 2 == 0;
+            }
+            public int GetValue(char symbol)
+            {
+                return _values[symbol];
+            }
+            private int CalculateValue(char symbol)
+            {
+                var index = Array.IndexOf(_symbols, symbol);
+
+                if (index == -1) throw new ArgumentOutOfRangeException(nameof(symbol), _symbols, string.Empty);
+
+                var tens = 1;
+                for (int i = 0; i < index / 2; i++)
+                {
+                    tens *= 10;
+                }
+                return IsPowerOfTen(symbol)
+                    ? 1 * tens
+                    : 5 * tens;
+            }
+
+            public bool IsMore(char left, char rigth)
+            {
+                return GetValue(left) > GetValue(rigth);
+            }
+
+            public bool IsLess(char left, char rigth)
+            {
+                return GetValue(left) < GetValue(rigth);
+            }
+        }
+    }
+
+    public interface IRomainNumerals
+    {
+        bool CanSubtract(char subtrahend, char minuend);
+        bool IsPowerOfTen(char symbol);
+        int GetValue(char symbol);
+        bool IsMore(char left, char rigth);
+        bool IsLess(char left, char rigth);
+    }
+}
--- RomanConverterTask/RomanConverterTask.Core/RomanConverter.cs
+++ RomanConverterTask/RomanConverterTask.Core/RomanConverter.cs
@@ -1,14 +1,138 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace RomanConverterTask.Core
 {
     public static class RomanConverter
     {
-        public static int RomanToInt(string s)
+        public static int RomanToInt(string roman)
         {
-            return 0;
+           
+            var minuendIndexes = GetMinuendIndexes(roman);
+            CkeckRepeat(roman, minuendIndexes);
+            CkeckDescending(roman, minuendIndexes);
+            var value = Calculate(roman, minuendIndexes);
+            if (value > 3000) throw new MaxValueException(3000, roman);
+            return value;
+        }
+        private static int Calculate(string roman, List<int> minuendIndexes)
+        {
+            var acc = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                var sing = !minuendIndexes.Contains(i)
+                    ? 1
+                    : -1;
+                acc += sing*RomainNumerals.Original.GetValue(roman[i]);
+            }
+            return acc;
+        }
+        private static List<int> GetMinuendIndexes(string roman)
+        {
+            var minuendIndexes = new List<int>();
+            for (int i = 1; i < roman.Length; i++)
+            {
+                var cur = roman[i];
+                var prev = roman[i - 1];
+                if (RomainNumerals.Original.IsMore(cur, prev))
+                {
+                    if (!RomainNumerals.Original.CanSubtract(cur, prev))
+                    {
+                        throw new SubtractException($"Must't {cur} minus {prev}", i - 1, roman);
+                    }
+                    else if (i - 2 != -1 && RomainNumerals.Original.IsMore(cur, roman[i - 2]))
+                    {
+                        throw new SubtractException($"Must't twice subtract", i - 1, roman);
+                    }
+                        else if (roman.Length != i + 1 && !RomainNumerals.Original.IsMore(prev, roman[i + 1]))
+                    {
+                        throw new SubtractException($"Must't subtract previous two more numbers", i - 1, roman);
+                    }
+                    else
+                    {
+                        minuendIndexes.Add(i - 1);
+                    }
+                }
+            }
+            return minuendIndexes;
+        }
+        private static void CkeckRepeat(string roman, List<int> minuendIndexes)
+        {
+            if (roman.Length < 2) return;
+            var counter = 0;
+            var canRepeat = false;
+            var prevSymbol = null as char?;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (!minuendIndexes.Contains(i))
+                {
+                    var cur = roman[i];
+                    if (prevSymbol == cur)
+                    {
+                        if (!canRepeat || (counter == 3 && !minuendIndexes.Contains(i-1)))
+                        {
+                            throw new RepeatException(i, roman);
+                        }
+                        counter++;
+                    }
+                    else
+                    {
+                        counter = 1;
+                        prevSymbol = cur;
+                        canRepeat = RomainNumerals.Original.IsPowerOfTen(cur);
+                    }
+                }
+
+               
+            }
+        }
+        private static void CkeckDescending(string roman, List<int> minuendIndexes)
+        {
+            for (int i = 1; i < roman.Length; i++)
+            {
+                if (minuendIndexes.Contains(i)) continue;
+                if (i - 2 == -1 && minuendIndexes.Contains(i - 1)) continue;
+
+                var cur = roman[i];
+                var prevIndex = !minuendIndexes.Contains(i-1)
+                    ? i - 1
+                    : i - 2;
+                var prev = roman[prevIndex];
+                if (RomainNumerals.Original.IsMore(cur, prev))
+                {
+                    throw new ValueDescendingException(prevIndex, roman);
+                }
+            }
+        }
+        
+    }
+
+    public class RepeatException : Exception
+    {
+        public RepeatException(int index, string roman) : base($"Limit is exceeded\r\nPosition {index} of {roman}")
+        {
         }
     }
+
+    public class SubtractException : Exception
+    {
+        public SubtractException(string message, int index, string roman) : base($"{message}\r\nPosition {index} of {roman}")
+        {
+        }
+    }
+
+    public class ValueDescendingException : Exception
+    {
+        public ValueDescendingException(int index, string roman) : base($"Value is not descending\r\nPosition {index} of {roman}")
+        {
+        }
+    }
+
+    public class MaxValueException : Exception
+    {
+        public MaxValueException(int value, string roman) : base($"Reached limit of {value} by {roman}")
+        {
+        }
+    }
+
 }
